979adaf9ca14044cbd136b53c37f52e18e78a602
Greg Valcourt <83992659+GValcourt@users.noreply.github.com>
2023-01-09 13:11:50 -0500

    Added my name to the web app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Created from the create-react-app project by Meta by Gregory Valcourt
         </p>
         <a
           className="App-link"
