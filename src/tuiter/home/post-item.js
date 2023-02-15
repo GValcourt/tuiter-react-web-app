311a9e59a9cf600c6a778addf867cb9b18cc7f2a
GValcourt <gregory.valcourt@gmail.com>
2023-02-15 16:25:45 -0500

    Implemented challenges
    Just need to clean up styles

---
+++ src/tuiter/home/post-item.js
@@ -0,0 +1,73 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const PostItem = (
+ {
+   post = {
+     "id" : 0,
+     "userName": "SpaceX",
+     "handle": "SpaceX",
+     "avatarIcon": "spacex.jpg",
+     "time": "2h",
+     "text": "Tesla Cybertruck lands on Mars and picks up the Curiosity rover on its 6' bed",
+     "image": "starship_center.jpg",
+     "comments":"4.2K",
+     "retuits":"3.5K",
+     "saves":"37.5K",
+     "retuit":true,
+     "retuitUsername":"Elon Musk",
+     "retuitId": -1,
+   }
+ }
+) => {
+  const fullwidth = {'width':'100%'} //TODO make CSS
+  const px48width = {'max_width':'48px'}
+
+  const retuitFormat = () =>{
+    return(
+      ` ${post.retuitUsername} Retuited`
+    );
+  };
+
+ return(
+  <li className="list-group-item border">
+   <div className="row">
+   <div className="col-2 text-center" style={px48width}>
+      {post.retuit ? <div className="ms-4"><i class="bi bi-megaphone"></i></div>: ''}
+      <img className="rounded-circle" height={48} width={48} src={`/images/${post.avatarIcon}`} alt={post.handle}/> 
+    </div>
+    <div className="col-10 ps-0">
+    <div className="fw-bold" style={{'font-size':'.7rem'}}>{post.retuit ? retuitFormat() : ''}</div>
+      <div><b className="fw-bolder">{post.userName} </b>
+        <i className="bi bi-check-circle-fill text-primary"></i> <b className="fw-light"> @{post.handle} &#x2022; {post.time}</b>
+        <div className="float-end"><i className="bi bi-three-dots"></i></div>
+      </div>
+      
+      <div>{post.text}</div>
+      <img className="float-center rounded-3 mt-2" style={fullwidth} src={`/images/${post.image}`}/>
+      <div className="row my-2 p-1 text-nowrap">
+        <div className="col">
+          <p>
+            <i className="bi bi-chat"></i>&#8195;{post.comments}
+            </p>
+        </div>
+        <div className="col">
+          <i class="bi bi-megaphone"></i>&#8195;{post.retuits}
+        </div>
+        <div className="col">
+          <i className="bi bi-heart"></i>&#8195;{post.saves}
+        </div>
+        <div className="col ms-3">
+          <i className="bi bi-upload"></i>
+        </div>
+      </div>
+      <div className="mb-3">
+        {post.retuit ? <Link>Show this thread</Link> : ''}
+      </div>
+    </div>
+    
+   </div>
+  </li>
+ );
+};
+export default PostItem;
--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -10,52 +10,52 @@ const NavigationSidebar = () => {
 return (
    <div className="list-group">
      <a className="list-group-item">
-      <div class="d-block d-xl-none"><i className="bi bi-fonts"></i></div>
-        <div class="d-none d-xl-block d-xxl-block"><i className="bi bi-fonts"></i> Tuiter</div> </a>
+      <div className="d-block d-xl-none"><i className="bi bi-fonts"></i></div>
+        <div className="d-none d-xl-block d-xxl-block"><i className="bi bi-fonts"></i> Tuiter</div> </a>
      <Link to="/tuiter/home" className={`list-group-item
-                    ${active === ''?'active':''}`}>
-        <div class="d-block d-xl-none"><i className="bi bi-house-fill"></i></div>
-        <div class="d-none d-xl-block d-xxl-block"><i className="bi bi-house-fill"></i> Home</div>
+                    ${active === '' || active === 'home'?'active':''}`}>
+        <div className="d-block d-xl-none"><i className="bi bi-house-fill"></i></div>
+        <div className="d-none d-xl-block d-xxl-block"><i className="bi bi-house-fill"></i> Home</div>
      </Link>
      <Link to="/tuiter/explore" className={`list-group-item
                     ${active === 'explore'?'active':''}`}>
-        <div class="d-block d-xl-none"><i className="bi bi-hash"></i></div>
-        <div class="d-none d-xl-block d-xxl-block"><i className="bi bi-hash"></i> Explore</div>
+        <div className="d-block d-xl-none"><i className="bi bi-hash"></i></div>
+        <div className="d-none d-xl-block d-xxl-block"><i className="bi bi-hash"></i> Explore</div>
      </Link>
      <Link to="/" className={`list-group-item
                     ${active === 'labs'?'active':''}`}>
-        <div class="d-block d-xl-none"><i class="bi bi-wrench"></i></div>
-        <div class="d-none d-xl-block d-xxl-block"><i class="bi bi-wrench"></i> Labs</div>
+        <div className="d-block d-xl-none"><i className="bi bi-wrench"></i></div>
+        <div className="d-none d-xl-block d-xxl-block"><i className="bi bi-wrench"></i> Labs</div>
      </Link>
      <a className={`list-group-item
                     ${active === 'notifications'?'active':''}`}>
-        <div class="d-block d-xl-none"><i className="bi bi-bell-fill"></i></div>
-        <div class="d-none d-xl-block d-xxl-block"><i className="bi bi-bell-fill"></i> Notifications</div>
+        <div className="d-block d-xl-none"><i className="bi bi-bell-fill"></i></div>
+        <div className="d-none d-xl-block d-xxl-block"><i className="bi bi-bell-fill"></i> Notifications</div>
      </a>
      <a className={`list-group-item
                     ${active === 'messages'?'active':''}`}>
-        <div class="d-block d-xl-none"><i className="bi bi-envelope-fill"></i></div>
-        <div class="d-none d-xl-block d-xxl-block"><i className="bi bi-envelope-fill"></i> Messages</div>
+        <div className="d-block d-xl-none"><i className="bi bi-envelope-fill"></i></div>
+        <div className="d-none d-xl-block d-xxl-block"><i className="bi bi-envelope-fill"></i> Messages</div>
      </a>
      <a className={`list-group-item
                     ${active === 'bookmarks'?'active':''}`}>
-        <div class="d-block d-xl-none"><i className="bi bi-bookmark-fill"></i></div>
-        <div class="d-none d-xl-block d-xxl-block"><i className="bi bi-bookmark-fill"></i> Bookmarks</div>
+        <div className="d-block d-xl-none"><i className="bi bi-bookmark-fill"></i></div>
+        <div className="d-none d-xl-block d-xxl-block"><i className="bi bi-bookmark-fill"></i> Bookmarks</div>
      </a>
      <a className={`list-group-item
                     ${active === 'lists'?'active':''}`}>
-       <div class="d-block d-xl-none"><i className="bi bi-list-ul"></i></div>
-        <div class="d-none d-xl-block d-xxl-block"><i className="bi bi-list-ul"></i> Lists</div>
+       <div className="d-block d-xl-none"><i className="bi bi-list-ul"></i></div>
+        <div className="d-none d-xl-block d-xxl-block"><i className="bi bi-list-ul"></i> Lists</div>
      </a>
      <a className={`list-group-item
                     ${active === 'profile'?'active':''}`}>
-        <div class="d-block d-xl-none"><i className="bi bi-person-fill"></i></div>
-        <div class="d-none d-xl-block d-xxl-block"><i className="bi bi-person-fill"></i> Profile</div>
+        <div className="d-block d-xl-none"><i className="bi bi-person-fill"></i></div>
+        <div className="d-none d-xl-block d-xxl-block"><i className="bi bi-person-fill"></i> Profile</div>
      </a>
      <a className={`list-group-item
                     ${active === 'more'?'active':''}`}>
-        <div class="d-block d-xl-none"><i className="bi bi-three-dots"></i></div>
-        <div class="d-none d-xl-block d-xxl-block"><i className="bi bi-three-dots"></i> More</div>
+        <div className="d-block d-xl-none"><i className="bi bi-three-dots"></i></div>
+        <div className="d-none d-xl-block d-xxl-block"><i className="bi bi-three-dots"></i> More</div>
      </a>
    </div>
  );
