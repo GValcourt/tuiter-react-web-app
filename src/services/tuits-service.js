4a9a1d89f7793d606e5ba328b3b19beca1b3e5fe
GValcourt <gregory.valcourt@gmail.com>
2023-03-27 21:18:24 -0400

    trying to fix access
    not connecting to server

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_MONGOOSE;
+//console.log(process.env.REACT_APP_API_MONGOOSE)
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
